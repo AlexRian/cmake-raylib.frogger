daf62c063ed036e9513b6ec640ab32414f2d87ca
Alexander Kulguskin <alexrians@gmail.com>
2023-06-26 19:45:36 +0300

    Cleaned code

--- sources/main.cpp
+++ sources/main.cpp
@@ -6,33 +6,15 @@
 
 int main(void)
 {
-    InitWindow(Settings::screenWidth, Settings::screenHeight, "Game");
+    InitWindow(Settings::screenWidth, Settings::screenHeight, "Frogger");
     SetTargetFPS(Settings::fps);
 
     Debug debug{ 0, 0, false };
-    Physics physics{ b2Vec2{0.f, 9.8f} };
-
-    Block box{ 
-        Settings::screenWidth / 2, 90, 90,
-        physics.getBody("Block", Settings::screenWidth / 2, 200.0f, 90, 200, 120, true, Box, {.5f, 0.3f, 0})
-    };
-
-    Ground ground{ 
-        Settings::screenWidth / 2, Settings::screenHeight, 180,
-        physics.getBody("Ground", Settings::screenWidth / 2, Settings::screenHeight, 180, Settings::screenWidth, 60, false, Box, {0.1f, 0.1f, 0.1f})
-    };
 
     while (!WindowShouldClose())
     {
-        physics.makeWorldStep();
-
-        box.applyPhysicsPosition();
-        ground.applyPhysicsPosition();
-
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            box.draw();
-            ground.draw();
             debug.draw();
         EndDrawing();
     }
